b0232ac7fbfe62dfb488c52da5632f4398daa3d6
Noah Holt <tidalturtles@gmail.com>
2023-09-29 15:12:41 -0600

    Began filling out webpage

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add your apps here
+    'portfolio_app',
 ]
+
+# Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
